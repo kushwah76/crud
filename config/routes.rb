579d15a3d29265ea6813de6563735466f38497b7
kushwah76 <ankushwah@bestpeers.com>
2023-01-09 19:20:16 +0530

    Crud_oprestions

---
+++ config/routes.rb
@@ -0,0 +1,17 @@
+Rails.application.routes.draw do
+  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+
+  # Defines the root path route ("/")
+  # root "articles#index"
+  #*************************************
+
+  # root "articles#index"
+  # get "/articles", to: "articles#index"
+  # get "/articles/:id", to: "articles#show"
+  
+  #*********************************
+  #esourceful Routing
+  root "articles#index"
+
+  resources :articles
+end
